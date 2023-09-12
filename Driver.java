8eaed85eab846b336f76f736c29b257bb860416b
Cameron <77825507+CameronWDavis@users.noreply.github.com>
2023-09-11 20:13:04 -0400

    Add files via upload
    updates made to driver

--- Driver.java
+++ Driver.java
@@ -1,10 +1,28 @@
 import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
 
 public class Driver {
 
     public static void main(String[] args) {
+        //this reads in standard input
         ANTLRInputStream input = new ANTLRInputStream(System.in);
 
+        //lexical analyzer
+        LittleLexer lexer = new LittleLexer(input);
+
+        //token generation
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        
+    }
+
+    private static void outputTokens(lexer,tokens){
+        int i = 0;
+        while(tokens[0] != null){
+            System.out.println("Token Type: " + LittleLexer.VOCABULARY.getDisplayName(tokens.getType()));
+            System.out.println("Value: " + tokens.getText());
+            tokens.consume();
+            i++; //iterate over
+        }
     }
 
 }
