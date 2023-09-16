0f338c2755c8055e63fc076ab06b67db8af8f93f
Cameron <77825507+CameronWDavis@users.noreply.github.com>
2023-09-16 19:05:08 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,41 +1,23 @@
+import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.CommonTokenStream;
-import java.io.FileInputStream;
-import java.io.File;
 import org.antlr.v4.runtime.Token;
+import java.io.IOException;
 
 public class Driver {
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws IOException {
+        ANTLRInputStream input = new ANTLRInputStream(System.in);
+        LittleLexer  lexer = new LittleLexer (input);
 
-        String fileName = "input.txt";
-        File file = new File(fileName);
-        FileInputStream fis = null;
-
-        fis = new FileInputStream(file);
-
-        ANTLRInputStream input = new ANTLRInputStream(fis);
-
-        //lexical analyzer
-        LittleLexer lexer = new LittleLexer(input);
-
-        //token generation
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-        // Generate tokens
-        tokens.fill();
-        printTokens(tokens,lexer);
+        processTokens(lexer);
     }
 
-         //This is a function to print tokens created by the lexical rules
-        private static void printTokens(CommonTokenStream tokens,LittleLexer lexer){
-            for (Token token : tokens.getTokens()) {
-                String tokenName = LittleLexer.VOCABULARY.getSymbolicName(token.getType());
-                String tokenValue = token.getText();
+    private static void processTokens(Little lexer) {
+        for (Token token = lexer.nextToken(); token.getType() != Token.EOF; token = lexer.nextToken()) {
+            String tokenName = Little.VOCABULARY.getDisplayName(token.getType());
+            String tokenValue = token.getText();
 
-                System.out.println("Token Type: " + tokenName);
-                System.out.println("Value: " + tokenValue);
-            }
+            System.out.println("Token Type: " + tokenName);
+            System.out.println("Value: " + tokenValue);
         }
-
-
+    }
 }
