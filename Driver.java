97497f2e074c561d829797af68240b6521d512c1
Cameron <77825507+CameronWDavis@users.noreply.github.com>
2023-09-17 11:48:39 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,17 +1,24 @@
+//imports for antlr libraries 
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.Token;
 import java.io.IOException;
 
 public class Driver {
+    //thows exception incase cant find file
     public static void main(String[] args) throws IOException {
+
+        //this tells ANTLR the input and what the lexer should analyize 
         ANTLRInputStream input = new ANTLRInputStream(System.in);
         LittleLexer  lexer = new LittleLexer (input);
 
+        //function call for printing 
         printTokens(lexer);
     }
 
+    /
     private static void printTokens(Little lexer) {
+        //for loop to print token and type
         for (Token token = lexer.nextToken(); token.getType() != Token.EOF; token = lexer.nextToken()) {
             String tokenName = Little.VOCABULARY.getDisplayName(token.getType());
             String tokenValue = token.getText();
