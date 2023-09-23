98f95a911485e14cf3e0c9ff7e0665b6e434c140
Cameron <77825507+CameronWDavis@users.noreply.github.com>
2023-09-23 16:52:46 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -3,18 +3,48 @@ import org.antlr.v4.runtime.*;
 import java.io.IOException;
 
 public class Driver {
+
+    static class ErrorCheck extends BaseErrorListener {
+        private static boolean error = false;
+
+        public void syntaxError(Recognizer<?, ?> recognizer,
+                                Object offendingSymbol,
+                                int line, int charPositionInLine,
+                                String msg,
+                                RecognitionException e) {
+            error = true;
+
+        }
+        public static boolean getError(){
+            return error;
+        }
+    }
+    
     //thows exception incase cant find file
     public static void main(String[] args) throws IOException {
 
         //this tells ANTLR the input and what the lexer should analyize 
         ANTLRInputStream input = new ANTLRInputStream(System.in);
         LittleLexer  lexer = new LittleLexer (input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        LittleParser parser = new LittleParser(tokens);
+        parser.addErrorListener(new ErrorCheck());
+
+        ParseTree tree = parser.program();
+
+
+
+         if (ErrorCheck.getError() == false) {
+                System.out.println("Accepted");
+            } else {
+            System.out.println("Not accepted");
+        }
 
         //function call for printing 
-        printTokens(lexer);
+       // printTokens(lexer);
     }
 
-    
+    //Function no longer in use right now 
     private static void printTokens(LittleLexer lexer) {
         //for loop to print token and type
         for (Token token = lexer.nextToken(); token.getType() != Token.EOF; token = lexer.nextToken()) {
