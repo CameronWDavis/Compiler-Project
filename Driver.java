f3a2c26b10462ff780aecbb6d1082c902f95066f
Cameron <77825507+CameronWDavis@users.noreply.github.com>
2023-09-27 20:11:27 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,11 +1,12 @@
-//imports for antlr libraries 
+//imports for antlr libraries
 import org.antlr.v4.runtime.*;
 import java.io.IOException;
 import org.antlr.v4.runtime.tree.*;
 
+
 public class Driver {
 
-    //this class is used to see if the inputed file is valid or not
+//this class is used to see if the inputed file is valid or not
     static class ErrorCheck extends BaseErrorListener {
         private static boolean error = false;
 
@@ -22,42 +23,21 @@ public class Driver {
             return error;
         }
     }
-    
-    //thows exception incase cant find file
-    public static void main(String[] args) throws IOException {
 
-        //this tells ANTLR the input and what the lexer should analyize 
+public static void  main(String[] args) throws IOException {
+
+//this tells ANTLR the input and what the lexer should analyize 
         ANTLRInputStream input = new ANTLRInputStream(System.in);
         LittleLexer  lexer = new LittleLexer (input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         LittleParser parser = new LittleParser(tokens);
         parser.addErrorListener(new ErrorCheck());
-
         ParseTree tree = parser.program();
 
-
-
-         if (ErrorCheck.getError() == false) {
+	if (ErrorCheck.getError() == false) {
                 System.out.println("Accepted");
             } else {
             System.out.println("Not accepted");
         }
-
-        //function call for printing tokens not in use right now 
-       // printTokens(lexer);
-    }
-
-    //Function no longer in use right now 
-    /* 
-    private static void printTokens(LittleLexer lexer) {
-        //for loop to print token and type
-        for (Token token = lexer.nextToken(); token.getType() != Token.EOF; token = lexer.nextToken()) {
-            String tokenName = Little.VOCABULARY.getDisplayName(token.getType());
-            String tokenValue = token.getText();
-
-            System.out.println("Token Type: " + tokenName);
-            System.out.println("Value: " + tokenValue);
-        }
-    }
-    */
+}
 }
