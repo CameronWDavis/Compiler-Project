d2aa367eae6b870bf9214ea47d833ff3b1f6ca80
Cameron <77825507+CameronWDavis@users.noreply.github.com>
2023-10-29 12:27:05 -0400

    Update Driver.java
    The changes add update the Driver.java with semantics

--- Driver.java
+++ Driver.java
@@ -6,38 +6,25 @@ import org.antlr.v4.runtime.tree.*;
 
 public class Driver {
 
-//this class is used to see if the inputed file is valid or not
-    static class ErrorCheck extends BaseErrorListener {
-        private static boolean error = false;
-
-        public void syntaxError(Recognizer<?, ?> recognizer,
-                                Object offendingSymbol,
-                                int line, 
-                                int charPositionInLine,
-                                String msg,
-                                RecognitionException e) {
-            error = true;
-
-        }
-        public static boolean getError(){
-            return error;
-        }
-    }
-
 public static void  main(String[] args) throws IOException {
 
 //this tells ANTLR the input and what the lexer should analyize 
         ANTLRInputStream input = new ANTLRInputStream(System.in);
+
+	//creation of lexer this turns text into lexical tokens 
         LittleLexer  lexer = new LittleLexer (input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+	//creation of parser this turns our tokens into formal grammar also sees if file is valid
         LittleParser parser = new LittleParser(tokens);
         parser.addErrorListener(new ErrorCheck());
         ParseTree tree = parser.program();
+	ParseTreeWalker walker = new ParseTreeWalker(); //parse the program 
 
-	if (ErrorCheck.getError() == false) {
-                System.out.println("Accepted");
-            } else {
-            System.out.println("Not accepted");
-        }
+	//creation of symbol table this gives our words meaning 
+	SimpleTableBuilder stb = new SimpleTableBuilder(); 
+	walker.walk(stb,tree); 
+	stb.prettyPrint(); //print the table 
+	
 }
 }
