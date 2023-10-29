1d6ca7dd1fb536c3014f2b63b2afd55136cc6325
Cameron <77825507+CameronWDavis@users.noreply.github.com>
2023-10-29 14:18:09 -0400

    Update SimpleTableBuilder.java

--- SimpleTableBuilder.java
+++ SimpleTableBuilder.java
@@ -1,23 +1,25 @@
 import java.util.*;
 
-class SymbolTable {
+class SymbolTable { //this class creates a symbol table using a hashmap
+
+    //class variables
     public String scope;
     public Map<String, Map<String, String>> table;
 
     public String getScope(){
-        return this.scope;
+        return this.scope; // method ot return scope
     }
 
-    public SymbolTable(String scope) {
+    public SymbolTable(String scope) { //constructor for symbole table
         this.scope = scope;
         this.table = new LinkedHashMap<>();
     }
 
-    public void addSymbol(String name, String type) {
+    public void addSymbol(String name, String type) {// add a symbol to ttable
         addSymbol(name, type, null);
     }
 
-    public void addSymbol(String name, String type, String value) {
+    public void addSymbol(String name, String type, String value) { //add symbol and throw exception if needed
         if (table.containsKey(name)) {
             System.out.println("DECLARATION ERROR " + name);
             throw new IllegalArgumentException("DECLARATION ERROR " + name);
@@ -29,6 +31,8 @@ class SymbolTable {
         }
     }
 
+
+    //print the table out
     public void printTable() {
         System.out.println("Symbol table " + this.scope);
         for (Map.Entry<String, Map<String, String>> entry : table.entrySet()) {
@@ -43,20 +47,29 @@ class SymbolTable {
             }
         }
     }
-}
+} // end of SymboleTable
 
 public class SimpleTableBuilder extends LittleBaseListener {
+    //class variables
     ArrayList <SymbolTable> stack = new ArrayList<SymbolTable>();
     SymbolTable table = new SymbolTable("temp");
     int blockCount = 1 ;
 
-
+    //add a table tp the stack
     public boolean pushStack(SymbolTable table) {
         if (stack.stream().noneMatch(tempTable -> tempTable.scope.equals(table.scope))) {
             stack.add(table);
             return true;
         }
         return false;
+    }
+
+    public void retrieveTableState(){ //method to get the state of the table 
+        if(pushStack(table)){
+            table = stack.get(stack.size() - 2);
+        }else{
+            table = stack.get(stack.size() - 1 );
+        }
     }
 
 
@@ -66,8 +79,20 @@ public class SimpleTableBuilder extends LittleBaseListener {
         pushStack(table);
     }
 
-    
+    @Override public void exitProgram(LittleParser.ProgramContext ctx) { //nothing else needed
+         }
 
+    @Override public void enterFunc_decl(LittleParser.Func_declContext ctx) {
+        retrieveTableState();
+        String name = ctx.id().getText();
+        SymbolTable funcTable = new SymbolTable(name);
+        table = funcTable;//create table for function
+    }
+
+    @Override public void exitFunc_decl(LittleParser.Func_declContext ctx) {
+        retrieveTableState();
+    }
+    
     @Override
     public void enterString_decl(LittleParser.String_declContext ctx) {
         String name = ctx.id().getText();
@@ -78,27 +103,22 @@ public class SimpleTableBuilder extends LittleBaseListener {
 
     @Override
     public void exitString_decl(LittleParser.String_declContext ctx){
-
+    //empty not needed
     }
 
 
-    @Override public void enterVar_decl(LittleParser.Var_declContext ctx){
+    @Override
+    public void enterVar_decl(LittleParser.Var_declContext ctx) {
         String type = ctx.var_type().getText();
-        if(type.compareTo("INT") == 0 ){
-            String name = ctx.var_type().getText();
-            String[] intValue = ctx.id_list().getText().split(",");
-            for(int i = 0; i < intValue.length; i++){
-                table.addSymbol(intValue[i], type);
-            }
-        }
-        else if (type.compareTo("FLOAT") == 0) {
-            String name = ctx.id_list().getText();
-            String[] intValue = ctx.id_list().getText().split(",");
-            for(int i = 0; i < intValue.length; i++){
-                table.addSymbol(intValue[i], type);
+        if ("INT".equals(type) || "FLOAT".equals(type)) {
+            String[] variableNames = ctx.id_list().getText().split(",");
+            for (String variableName : variableNames) {
+                table.addSymbol(variableName.trim(), type);
             }
         }
     }
+
+    @Override public void exitVar_decl(LittleParser.Var_declContext ctx) { }
 
     @Override public void enterParam_decl(LittleParser.Param_declContext ctx) {
         String type = ctx.var_type().getText();
@@ -107,11 +127,6 @@ public class SimpleTableBuilder extends LittleBaseListener {
     }
 
     @Override public void exitParam_decl(LittleParser.Param_declContext ctx) { }
-
-    public String getBlock() {
-        return "BLOCK " + blockCount++;
-    }
-
 
     @Override public void enterIf_stmt(LittleParser.If_stmtContext ctx){
         if(pushStack(table)){
@@ -122,73 +137,36 @@ public class SimpleTableBuilder extends LittleBaseListener {
                 blockCount--;
             }
         }
-        SymbolTable blockTable = new SymbolTable(getBlock());
+        SymbolTable blockTable = new SymbolTable("BLOCK " + blockCount++);
         table = blockTable;
     }
 
     @Override public void exitIf_stmt(LittleParser.If_stmtContext ctx){
-        if(pushStack(table)) {
-            table = stack.get(stack.size() - 2);
-        }else {
-            table = stack.get(stack.size() - 1);
-        }
+        retrieveTableState();
     }
 
     @Override public void enterElse_part(LittleParser.Else_partContext ctx){
-        if(pushStack(table)){
-            table = stack.get(stack.size() - 2);
-        }else{
-            table = stack.get(stack.size() - 1 );
-        }
-        SymbolTable blockTable = new SymbolTable(getBlock());
+        retrieveTableState();
+        SymbolTable blockTable = new SymbolTable("BLOCK " + blockCount++);
         table = blockTable;
     }
 
     @Override public void exitElse_part(LittleParser.Else_partContext ctx) { }
 
     @Override public void enterWhile_stmt(LittleParser.While_stmtContext ctx) {
-        if(pushStack(table)){
-            table = stack.get(stack.size() - 2);
-        }else {
-            table = stack.get(stack.size() - 1);
-        }
-        SymbolTable blockTable = new SymbolTable(getBlock());
+        retrieveTableState();
+        SymbolTable blockTable = new SymbolTable("BLOCK " + blockCount++);
         table = blockTable;
     }
 
     @Override public void exitWhile_stmt(LittleParser.While_stmtContext ctx) {
-        if(pushStack(table)){
-            table = stack.get(stack.size() - 2);
-        }else {
-            table = stack.get(stack.size() - 1);
-        }
+        retrieveTableState();
     }
 
-    @Override public void enterFunc_decl(LittleParser.Func_declContext ctx) {
-        if(pushStack(table)){
-            table = stack.get(stack.size() - 2);
-        }else {
-            table = stack.get(stack.size() - 1);
-        }
-        String name = ctx.id().getText();
-        SymbolTable funcTable = new SymbolTable(name);
-        table = funcTable;
-    }
-
-    @Override public void exitFunc_decl(LittleParser.Func_declContext ctx) {
-        if(pushStack(table)){
-            table = stack.get(stack.size() - 2);
-        }else {
-            table = stack.get(stack.size() - 1);
-        }
-
-    }
-
-    public void prettyPrint(){
+    public void prettyPrint(){ //print the table after its been processed
         for (SymbolTable currentTable : stack){
             currentTable.printTable();
             System.out.println();
         }
     }
-
-}
+}//end Simpletablebuilder 
